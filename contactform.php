50f36ddcd0c60fda23265506058290f852ffc4ec
Adi Gudi <agudi24@gmail.com>
2019-12-30 16:56:52 -0800

    hshs

--- contactform.php
+++ contactform.php
@@ -9,5 +9,5 @@
  	$headers = "From: ".$mailFrom;
  	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
  	mail($mailTo, $subject, $txt, $headers);
- 	header("Location; index.php?mailsend");
+ 	header("Location; page4.html?mailsend");
  }
