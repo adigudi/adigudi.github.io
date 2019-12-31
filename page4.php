7b33d342f63fc237178c41d64bca6143b5ad507a
Adi Gudi <agudi24@gmail.com>
2019-12-30 16:12:56 -0800

    added php stuff

--- page4.php
+++ page4.php
@@ -6,13 +6,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="index.css">
-    <link rel="icon" href="/images/download.png">
 </head>
 
 <body>
 
     <div class="header">
-        <h1>ADI GUDI</h1>
+        <h1>Contact</h1>
     </div>
 
     <div class="topnav">
@@ -22,14 +21,16 @@
       <a href="page3.html">Blog</a>
       <a href="page4.php">Contact</a>
     </div>
-
-    <div class="row content">
-    <p class="center">Hi there and welcome to my personal website that I hard coded from scratch!</p>
-    <img src="/images/homepagepic.gif">
-    </div>
-
-    <div class="footer">
-        <p>Created by Adi Gudi</p>
-    </div>
-
+<main>
+  <p>Send Email<p>
+  <form class="contact-form" action="contactform.php" method="post">
+    <input type="text" name="name" placeholder="Full Name">
+    <input type="text" name="mail" placeholder="Your email">
+    <input type="text" name="subject" placeholder="Subject">
+    <textarea name="message"placeholder="Message"></textarea>
+    <button type="submit" name="submit">SEND MAIL</button>
+  </form>
+</main>
 </body>
+
+</html>
